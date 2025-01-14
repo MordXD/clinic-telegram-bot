fac85bafc13c6abae7c9107dc10ff9cf2580eca4
gelman <kotkinegor78@gmail.com>
2025-01-14 22:45:49 +0300

    Изменения README инстуркции к запуску

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -3,7 +3,6 @@ import coloredlogs
 from telegram import Update
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 from config import BOT_TOKEN, LOG_LEVEL
 from config import ADMIN_CHAT_ID
 from bot.handlers.admin_handlers import handle_admin_notifications
